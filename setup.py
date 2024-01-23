15577b3779a7fe1ade679840dc20a8b9d9c742ed
ShunyaYamagami <othello200063@gmail.com>
2024-01-23 15:55:33 +0900

    setup with pyproject

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from pathlib import Path
+# from pathlib import Path
 
-import pkg_resources
-from setuptools import find_packages, setup
+# import pkg_resources
+# from setuptools import find_packages, setup
 
-setup(
-    name="adaface",
-    version="0.1.0",
-    license="proprietary",
-    description="hogehoge",
-    author="naxa",
-    url="https://github.com/naxa/AdaFace",
-    packages=find_packages(),
-    install_requires=[
-        str(r) for r in pkg_resources.parse_requirements(Path(__file__).with_name("requirements.txt").open())
-    ],
-)
+# setup(
+#     name="adaface",
+#     version="0.1.0",
+#     license="proprietary",
+#     description="hogehoge",
+#     author="naxa",
+#     url="https://github.com/naxa/AdaFace",
+#     packages=find_packages(),
+#     install_requires=[
+#         str(r) for r in pkg_resources.parse_requirements(Path(__file__).with_name("requirements.txt").open())
+#     ],
+# )
